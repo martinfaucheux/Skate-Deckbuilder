700b5444121f9b48376fca0c80cf951e69db5ddc
Martin Faucheux <martin.faucheux@gmail.com>
2025-03-03 17:04:29 +0100

    place card based on slope start and end

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,6 +1,8 @@
 using System.Linq;
 using UnityEngine;
 using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Unity.VisualScripting;
 
 public class BoardManager : Singleton<BoardManager>
 {
@@ -43,8 +45,23 @@ public class BoardManager : Singleton<BoardManager>
 
         cards[cardIdx] = card;
         card.transform.SetParent(cardHolders[cardIdx]);
-        card.transform.localPosition = Vector3.zero;
+        Vector3 position = card.transform.parent.position;
+        card.transform.position = position;
+        UpdateCardsYAxis();
         return true;
+    }
+
+    private void UpdateCardsYAxis()
+    {
+        for (int cardIdx = 0; cardIdx < cardCount; cardIdx++)
+        {
+            if (cards[cardIdx] != null)
+            {
+                Vector3 position = cardHolders[cardIdx].position;
+                position.y = GetCardY(cards[cardIdx], cardIdx > 0 ? cards[cardIdx - 1] : null);
+                cards[cardIdx].transform.position = position;
+            }
+        }
     }
 
     public bool RemoveCard(Card card)
@@ -54,6 +71,7 @@ public class BoardManager : Singleton<BoardManager>
             return false;
 
         cards[cardIdx] = null;
+        UpdateCardsYAxis();
         return true;
     }
 
@@ -70,5 +88,20 @@ public class BoardManager : Singleton<BoardManager>
     }
 
     public bool CanAddCard() => cards.Any(card => card == null);
+
+
+    private float GetCardY(Card card, Card previousCard)
+    {
+        Vector3 localOffset = card.actionContainer.startTransform.localPosition;
+        // card actoinContainer start point must be same as previous card actionContainer end point
+        if (previousCard == null)
+        {
+            return -localOffset.y;
+        }
+
+        Vector3 previousPosition = previousCard.actionContainer.endTransform.position;
+        return (previousPosition - localOffset).y;
+
+    }
 
 }
