973bd8701f97ec1b8f64efe4e7ab642b64b42d82
Yohan Jabin <jabinyohan@gmail.com>
2025-03-05 00:07:46 +0100

    Add arrows to each card and add cinemachine to follow player

--- Assets/Scripts/ActionContainer.cs
+++ Assets/Scripts/ActionContainer.cs
@@ -7,6 +7,7 @@ public class ActionContainer : MonoBehaviour
     public QTEConfig qteConfig;
     public ChallengeDisplay challengeDisplay;
     public CardType cardType;
+    public SpriteRenderer arrowSpriteRenderer;
 
     void Awake()
     {
@@ -23,8 +24,34 @@ public class ActionContainer : MonoBehaviour
         if (challenge != null && challengeDisplay != null)
         {
             challengeDisplay.AssignChallenge(challenge);
+            SetArrowSprite(challenge);
         }
 
         return challenge;
+    }
+
+    public void SetArrowSprite(ActionSequenceChallenge challenge)
+    {
+        float angle = 0;
+
+        KeyCode key = challenge.keyCode;
+        if (key == KeyCode.RightArrow)
+        {
+            angle = 0;
+        }
+        else if (key == KeyCode.UpArrow)
+        {
+            angle = 90;
+        }
+        else if (key == KeyCode.LeftArrow)
+        {
+            angle = 180;
+        }
+        else if (key == KeyCode.DownArrow)
+        {
+            angle = 270;
+        }
+
+        arrowSpriteRenderer.transform.localRotation = Quaternion.Euler(0, 0, angle);
     }
 }
